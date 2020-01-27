b9020cb9d593490296b6d5a37d079b4375776e72
Binit Rai <binitcse@gmail.com>
2020-01-27 17:06:07 +0530

    Updates

--- precache-manifest.1dcfe7691d8a4b57f7f55839c9a280c9.js
+++ precache-manifest.1dcfe7691d8a4b57f7f55839c9a280c9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d9f9ff5914df8581c92d2770f4cf151b",
+    "revision": "891b96e3e304d915582c7cf0ff0da9aa",
     "url": "/todo-new/index.html"
   },
   {
-    "revision": "08643c9cb19e9be416cc",
+    "revision": "cc9c713b4b35563e2b08",
     "url": "/todo-new/static/css/main.1c40afe1.chunk.css"
   },
   {
-    "revision": "ddd48676c4b1980c985e",
-    "url": "/todo-new/static/js/2.357be383.chunk.js"
+    "revision": "f84dc99ce7ff4ba521c1",
+    "url": "/todo-new/static/js/2.0e27313e.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/todo-new/static/js/2.357be383.chunk.js.LICENSE"
+    "url": "/todo-new/static/js/2.0e27313e.chunk.js.LICENSE"
   },
   {
-    "revision": "08643c9cb19e9be416cc",
-    "url": "/todo-new/static/js/main.ceef5120.chunk.js"
+    "revision": "cc9c713b4b35563e2b08",
+    "url": "/todo-new/static/js/main.6852b7c7.chunk.js"
   },
   {
     "revision": "17ef8e2f2abf53cfbc75",
