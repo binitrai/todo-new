cf86bb609ab19746c03347170bb057f94c2773bb
Binit Rai <binitcse@gmail.com>
2020-01-27 16:45:00 +0530

    Updates

---
+++ precache-manifest.1dcfe7691d8a4b57f7f55839c9a280c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891b96e3e304d915582c7cf0ff0da9aa",
+    "url": "/todo-new/index.html"
+  },
+  {
+    "revision": "cc9c713b4b35563e2b08",
+    "url": "/todo-new/static/css/main.1c40afe1.chunk.css"
+  },
+  {
+    "revision": "f84dc99ce7ff4ba521c1",
+    "url": "/todo-new/static/js/2.0e27313e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todo-new/static/js/2.0e27313e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc9c713b4b35563e2b08",
+    "url": "/todo-new/static/js/main.6852b7c7.chunk.js"
+  },
+  {
+    "revision": "17ef8e2f2abf53cfbc75",
+    "url": "/todo-new/static/js/runtime-main.ec96b9f3.js"
+  }
+]);
